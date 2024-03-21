588c27d744c5c7de0e259baea79f2964cdc1cecd
jihwooon <jihwooon@gmail.com>
2024-03-21 16:52:15 +0900

    주문 엔티티 및 로직 구현
    - 주문 회원 (member), 배송 정보 (delivery), 주문 상품 목록 (orderItems) 필드 추가
    - 주문 생성 메서드 (createOrder) 추가
    - 주문 취소 메서드 (cancel) 추가
    - 주문 전체 가격 계산 메서드 (getTotalPrice) 추가

--- src/main/java/jpa/core/domain/Order.java
+++ src/main/java/jpa/core/domain/Order.java
@@ -16,11 +16,13 @@ import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import jpa.core.domain.items.Item;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 @Table(name = "orders")
 public class Order {
 
@@ -58,5 +60,41 @@ public class Order {
     public void addOrderItem(OrderItem orderItem) {
         orderItems.add(orderItem);
         orderItem.setOrder(this);
+    }
+
+    /**
+     * 주문 생성
+     */
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setOrderStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getDeliveryStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setOrderStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    /**
+     * 주문 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        int totalPrice = orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
+        return totalPrice;
     }
 }
--- src/main/java/jpa/core/domain/OrderItem.java
+++ src/main/java/jpa/core/domain/OrderItem.java
@@ -13,7 +13,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 @Table(name = "order_item")
 public class OrderItem {
 
@@ -34,4 +35,35 @@ public class OrderItem {
     private int orderPrice;
 
     private int count;
+
+    //== 생성 메서드 ==//
+
+    /**
+     * 주문 상품 생성
+     */
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItems(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //== 비즈니스 로직 ==//
+
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        getItems().addStockQuantity(count);
+    }
+
+    /**
+     * 주문 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
