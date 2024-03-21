c15c573b51c0ec7816f9792cbb3573809b35aa9e
jihwooon <jihwooon@gmail.com>
2024-03-21 17:02:45 +0900

    JPA 주문 리포지토리 추가
    - OrderRepository.java (생성): Order 엔티티를 저장, 조회하는 기능을 제공하는 리포지토리 클래스 생성

---
+++ src/main/java/jpa/core/repository/OrderRepository.java
@@ -0,0 +1,22 @@
+package jpa.core.repository;
+
+import jakarta.persistence.EntityManager;
+import jpa.core.domain.Order;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+}
